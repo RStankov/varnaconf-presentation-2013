67fa19b7a94ffb0faefa02c631e4f8166d779b1a
Radoslav Stankov <rstankov@gmail.com>
2013-03-29 01:13:20 +0200

    Large class

--- app/models/exam_entry.rb
+++ app/models/exam_entry.rb
@@ -5,12 +5,4 @@ class ExamEntry < ActiveRecord::Base
   has_many :answers, class_name: 'ExamAnswer'
 
   validates_presence_of :user, :exam
-
-  def result
-    OpenStruct.new(
-      correct_answers: answers.select(&:correct?),
-      wrong_answers: answers.reject(&:correct?),
-      score: answers.to_a.sum(&:points)
-    )
-  end
 end
--- spec/models/exam_entry_spec.rb
+++ spec/models/exam_entry_spec.rb
@@ -1,28 +1,4 @@
 require 'spec_helper'
 
 describe ExamEntry do
-  describe "#result" do
-    it "works ( I hope :P )" do
-      exam = create :exam
-
-      single_1 = create :single_question, exam: exam, points: 1
-      single_2 = create :single_question, exam: exam, points: 2
-
-      multi_1 = create :multi_question, exam: exam, points: 1
-      multi_2 = create :multi_question, exam: exam, points: 2
-
-      entry = create(:exam_entry, answers: [
-        create(:answer, question: single_1, text: single_1.correct_answer),
-        create(:answer, question: single_2, text: '-wrong-answer-'),
-        create(:answer, question: multi_1,  text: multi_1.correct_answers.first),
-        create(:answer, question: multi_2,  text: '-wrong-answer-')
-      ])
-
-      result = entry.result
-
-      result.correct_answers.map(&:question).should eq [single_1, multi_1]
-      result.wrong_answers.map(&:question).should eq [single_2, multi_2]
-      result.score.should eq single_1.points + multi_1.points
-    end
-  end
 end
