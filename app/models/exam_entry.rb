6b645937289f6f68f7f30bf1810a8a9b2f750da2
Radoslav Stankov <rstankov@gmail.com>
2013-03-29 00:58:17 +0200

    Verbose code

--- app/models/exam_entry.rb
+++ app/models/exam_entry.rb
@@ -7,24 +7,10 @@ class ExamEntry < ActiveRecord::Base
   validates_presence_of :user, :exam
 
   def result
-    correct_answers = []
-    wrong_answers = []
-    score = 0
-
-    answers.each do |answer|
-      if answer.correct?
-        correct_answers << answer
-      else
-        wrong_answers << answer
-      end
-
-      score += answer.points
-    end
-
     {
-      correct_answers: correct_answers,
-      wrong_answers: wrong_answers,
-      score: score
+      correct_answers: answers.select(&:correct?),
+      wrong_answers: answers.reject(&:correct?),
+      score: answers.to_a.sum(&:points)
     }
   end
 end
